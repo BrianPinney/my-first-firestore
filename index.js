834efe1a74717e5ce168693d6be0278ee869bf21
Brian J. Pinney <brianj.pinney@gmail.com>
2023-03-29 15:49:56 -0400

    all work learning CRUD

--- index.js
+++ index.js
@@ -12,17 +12,52 @@ initializeApp({
 
 // Connect to firestore DB
 const db = getFirestore();
+//^^^^^ALL CONNECTING TO FIRESTORE^^^^^
 
 // Add a product to our product collection
-const candy= {
-    name: "skittles",
-    price: 3.99,
-    size: "16 oz",
-    color:  "green",
-    inventory: 144,
-    productNumber: 7,
-}
+// const candy2= {
+//     name: "Twix",
+//     price: 2.99,
+//     size: "12 oz",
+//     color:  "gold",
+//     inventory: 288,
+//     productNumber: 2,
+// }
 
-db.collection('products').add(candy)
-    .then(doc => console.log("added doc: " + doc.id))
-    .catch(err => console.log(err))
+//How to add a document to Firestore:
+// db.collection('products').add(candy2)
+//     .then((doc) => {console.log("added doc: " + doc.id)
+    
+// }) 
+//     .catch(err => console.log(err))
+
+// db.collection('products')
+//     .doc('AgADzrYZNeBz0eZpB5L2')
+//     .delete()
+
+// How to update a document in Firestore:
+
+db.collection('products')
+    .doc('AgADzrYZNeBz0eZpB5L2')
+    .update({
+        inventory: 555,
+        customerFavorite: true,
+    })
+
+// How to read a document in Firestore:
+db.collection('products')
+    .doc('AgADzrYZNeBz0eZpB5L2')
+    .get()
+.then(doc => {
+    console.log(doc.data())
+})
+.catch(console.log)
+
+//How to get a whole collection:
+
+db.collection('products').get()
+     .then(collection => {
+        const productList = collection.docs.map(doc => ({...doc.data(), id: doc.id }))
+        console.table(productList)
+     })
+     .catch(console.log)
